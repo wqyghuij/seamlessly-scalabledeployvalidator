02e9e79489306d575dd34f0e4df7237984a58bfc
wqyghuij <gs1sedabres@hotmail.com>
2024-05-07 02:14:24 +0700

    aptos

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-const name = "john";
-const upperName = name.toUpperCase();
-console.log(upperName); // JOHN
+function permuteUnique(nums) {
+  nums.sort((a, b) => a - b);
+  const result = [];
+  const used = new Array(nums.length).fill(false);
+  backtrack([]);
+  return result;
+  function backtrack(permutation) {
+    if (permutation.length === nums.length) {
+      result.push([...permutation]);
+      return;
+    }
+    for (let i = 0; i < nums.length; i++) {
+      if (used[i] || (i > 0 && nums[i] === nums[i - 1] && !used[i - 1]))
+        continue;
+      used[i] = true;
+      permutation.push(nums[i]);
+      backtrack(permutation);
+      permutation.pop();
+      used[i] = false;
+    }
+  }
+}
