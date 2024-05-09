21e8dbd4de77cd246b6469a4bc2b32e582652503
wqyghuij <gs1sedabres@hotmail.com>
2024-05-10 06:52:40 +0700

    demo project

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const selectionSort = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    let minIndex = i;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j;
-      }
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
     }
-    if (minIndex !== i) {
-      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
-    }
+    arr[j + 1] = key;
   }
   return arr;
-};
+}
